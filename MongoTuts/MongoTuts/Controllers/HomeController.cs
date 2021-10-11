8b7cb1c03988b1602d28670cd74ef9379b613bbf
Vishal Suthar <vishal@hotelops.in>
2021-10-11 13:29:36 +0530

    Mongo Tutorial

---
+++ MongoTuts/MongoTuts/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoTuts.DBController;
+
+namespace MongoTuts.Controllers
+{
+    public class HomeController : Controller
+    {
+        MongoDBController objM = new MongoDBController();
+        public ActionResult Index()
+        {
+
+            var DbList = objM.DBList();
+            TempData["DbList"] = DbList;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Name, string City, string Remarks)
+        {
+            var document = new MongoDB.Bson.BsonDocument();
+            document.Add(new MongoDB.Bson.BsonElement("Name", Name));
+            document.Add(new MongoDB.Bson.BsonElement("City", City));
+            document.Add(new MongoDB.Bson.BsonElement("Remarks", Remarks));
+            document.Add(new MongoDB.Bson.BsonElement("timestamp", DateTime.Now.Ticks));
+
+
+
+
+
+
+            objM.Insert(document, "test");
+
+
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MongoTuts/MongoTuts/DBController/MongoDBController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using System.Data;
+using System.Configuration;
+namespace MongoTuts.DBController
+{
+    public class MongoDBController
+    {
+        public dynamic DBList() {
+
+            string ConnString = ConfigurationManager.AppSettings["MongoConn"].ToString();
+            MongoClient dbClient = new MongoClient(ConnString);
+
+            List<MongoDB.Bson.BsonDocument> dbList = dbClient.ListDatabases().ToList();
+
+            return dbList;
+        }
+        public int Insert(MongoDB.Bson.BsonDocument bsonDocument, string collectionName) {
+            try
+            {
+                string ConnString = ConfigurationManager.AppSettings["MongoConn"].ToString();
+                MongoClient dbClient = new MongoClient(ConnString);
+                var database = dbClient.GetDatabase("testDB");//DateBaseName
+
+                var collection = database.GetCollection<MongoDB.Bson.BsonDocument>(collectionName);
+                collection.InsertOne(bsonDocument);
+                return 1;
+            }
+            catch (Exception e) { throw e; }
+            return -99;
+
+        }
+    }
+}
